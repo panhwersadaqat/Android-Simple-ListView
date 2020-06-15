458960ef034e7b6645dcaa4d03646fffe370d036
Sadaqat Panhwer <panhwersadaqat22@gmail.com>
2020-06-15 14:46:42 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Working With simple List view Demo App (Java)
+
+demo app for Beginners
+
+## Screenshots
+
+![Screenshot 1](screenshots/screen_1.png)
+![Screenshot 2](screenshots/screen_2.png)
+
---
+++ app/src/main/java/com/thefuturestic/listviewusingjava/MainActivity.java
@@ -0,0 +1,40 @@
+package com.thefuturestic.listviewusingjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    TextView textView;
+    String[] listItem;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        listView=(ListView)findViewById(R.id.listView);
+        textView=(TextView)findViewById(R.id.textView);
+        listItem = getResources().getStringArray(R.array.sample_array);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, listItem);
+        listView.setAdapter(adapter);
+
+        //adding listener on listview
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String value=adapter.getItem(position);
+                Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
